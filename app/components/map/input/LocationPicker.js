f9c2d8c52c9eb64cb060418cd06de81cfba68109
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-26 01:54:13 +0300

    Moving marker when location is selectted bt doesnt span however

--- app/components/map/input/LocationPicker.js
+++ app/components/map/input/LocationPicker.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Modal } from "react-native";
+import { StyleSheet, View, Text, Modal, Image } from "react-native";
 import React, { useEffect, useState } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import LocationChoice from "./LocationChoice";
@@ -6,31 +6,33 @@ import { IconButton, List, useTheme } from "react-native-paper";
 import useLocation from "../hooks/useLocation";
 import { SearchBar, SearchHeader } from "../../input";
 import { useGeoService } from "../../../api";
+import MapView, { Marker } from "react-native-maps";
 // import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const LocationPicker = ({ location, onLocationChange }) => {
   const [showModal, setShowModal] = useState(false);
-  const [deliverLocation, setDeliveryLocation] = useState();
   const { searchPlace } = useGeoService();
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const currLocation = useLocation();
   const { colors, roundness } = useTheme();
-  useEffect(() => {
-    if (deliverLocation) {
-      if (onLocationChange instanceof Function) {
-        onLocationChange(deliverLocation);
-      }
-    }
-  }, [deliverLocation]);
+  const [markerLocation, setMarkerLocation] = useState();
 
   useEffect(() => {
     handleSearch();
   }, [search]);
+  useEffect(() => {
+    if (currLocation) {
+      setMarkerLocation(currLocation);
+    }
+  }, [currLocation]);
 
   const handleSpanAndPlot = (selected) => {
     setSearch(selected.display);
-    console.log("Span and plot to map....", selected);
+    const {
+      coordinates: { lat: latitude, lng: longitude },
+    } = selected;
+    setMarkerLocation({ latitude, longitude });
   };
 
   const handleSearch = async () => {
@@ -39,6 +41,8 @@ const LocationPicker = ({ location, onLocationChange }) => {
       setSearchResults(response.data.results);
     }
   };
+
+  const coords = markerLocation || currLocation;
 
   return (
     <>
@@ -49,7 +53,7 @@ const LocationPicker = ({ location, onLocationChange }) => {
           color={colors.outline}
         />
         <Text style={[styles.input]}>
-          {location && (location.longitude || location.latitude)
+          {location?.latitude && location?.longitude
             ? `(${location.latitude}, ${location.longitude})`
             : "Choose Delivery Location"}
         </Text>
@@ -95,11 +99,35 @@ const LocationPicker = ({ location, onLocationChange }) => {
             />
           </View>
         </View>
-        <LocationChoice
-          defaultSpanLoc={location ? location : currLocation}
-          onLocationChosen={setDeliveryLocation}
-          defaultLocation={location ? location : currLocation}
-        />
+        {markerLocation && (
+          <View style={styles.mapContainer}>
+            <MapView
+              style={styles.map}
+              initialRegion={{
+                latitude: markerLocation.latitude,
+                longitude: markerLocation.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+            >
+              <Marker
+                coordinate={markerLocation }
+                title="Long press and drag to your desired location"
+                draggable
+                onDragEnd={(e) => {
+                  setMarkerLocation(e.nativeEvent.coordinate);
+                  if (onLocationChange instanceof Function)
+                    onLocationChange(e.nativeEvent.coordinate);
+                }}
+              >
+                <Image
+                  source={require("../../../assets/hospitalmarker.png")}
+                  style={{ width: 60, height: 60 }}
+                />
+              </Marker>
+            </MapView>
+          </View>
+        )}
       </Modal>
     </>
   );
@@ -108,6 +136,15 @@ const LocationPicker = ({ location, onLocationChange }) => {
 export default LocationPicker;
 
 const styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "red",
+  },
+  map: {
+    flex: 1,
+  },
   close: {
     alignSelf: "flex-end",
   },
