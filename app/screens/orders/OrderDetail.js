0eb6f17c500512b4514cbaee80470dca7291a668
Laurent Ouma <lawiomosh3@gmail.com>
2023-07-28 14:46:02 +0300

    Order detail reeiving detailed order though no widgets yet to dislay them

--- app/screens/orders/OrderDetail.js
+++ app/screens/orders/OrderDetail.js
@@ -1,52 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeArea } from "../../components/layout";
-import { usePatient } from "../../api";
 import { CardTitle } from "../../components/common";
 import { ActivityIndicator } from "react-native-paper";
 
 const OrderDetail = ({ navigation, route }) => {
   const { modes, timeSlots, methods, order } = route.params;
-  const { getAppointment, getOrder } = usePatient();
   const [loading, setLoading] = useState(false);
   const [currOrder, setCurrOrder] = useState(null);
 
-  const fetch = async () => {
-    setLoading(true);
-    const promises = Promise.all([
-      getOrder(order._id),
-      getAppointment(order.appointment),
-    ]);
-    const results = await promises;
-    const success = results.every((resp) => resp.ok);
-    setLoading(false);
-    if (success) {
-      setCurrOrder({ ...results[0].data, appointment: results[1].data });
-    }
-  };
-
-  useEffect(() => {
-    fetch();
-  }, []);
-
   return (
     <SafeArea>
-      {loading ? (
-        <>
-          <ActivityIndicator size={80} />
-        </>
-      ) : (
-        <>
-          {currOrder && (
-            <>
-              {console.log(currOrder)}
-              <CardTitle icon="cart" text="Order Id" subText={currOrder._id} />
-              <CardTitle icon="cart" text="Order Id" subText={currOrder._id} />
-              <CardTitle icon="cart" text="Order Id" subText={currOrder._id} />
-            </>
-          )}
-        </>
-      )}
+      <CardTitle text="Order id" subText={order._id} icon="cart" />
     </SafeArea>
   );
 };
